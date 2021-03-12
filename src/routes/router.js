f27b5e72ee32af35e09bb32074aa8b7cc527329b
mateo arteaga <mateoarteaga@mateos-Air.hitronhub.home>
2021-03-12 02:25:02 -0500

    cambie la version de vue router para la ultima para que fuera compatible con vue3 y cambie las paginas para views

---
+++ src/routes/router.js
@@ -0,0 +1,19 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Mateo from "../views/Mateo.vue";
+import Landing from "../views/Landing.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Landing,
+    name: "landind",
+  },
+  { path: "/mateo", component: Mateo, name: "mateo" },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
