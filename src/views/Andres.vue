ac26b0802b79638b4db780af19d713e7d46fde81
Andres Velez Zapata <andresvelez11@gmail.com>
2021-03-17 11:29:48 -0500

    Arregle interests

--- src/views/Andres.vue
+++ src/views/Andres.vue
@@ -41,7 +41,7 @@
     >
       <!-- Skills set -->
       <div class="flex flex-col justify-center my-8 items-center">
-        <PortfolioMainText text="Skills" class="text-white" />
+        <PortfolioMainText text="Skills" class="textbase-white" />
         <div class="w-full flex flex-wrap space-y-8">
           <div class="w-full">
             <SkillsType skillType="Languages" />
@@ -55,48 +55,48 @@
       class="w-full flex flex-col items-center justify-center mb-20 mt-10 px-10"
     >
       <PortfolioMainText text="Interests" class="text-black" />
-      <div class="w-full h-90 flex flex-row overflow-x-auto space-x-10">
+      <div class="w-full flex flex-wrap justify-around items-start">
         <div
-          class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+          class="flex flex-col items-center w-50 h-70 space-y-10 rounded-2xl shadow-lg"
         >
           <div class="lg:text-xl mt-5"><h2>Game Development</h2></div>
-          <div class="w-30 h-30">
+          <div class="w-25 h-25">
             <img src="../assets/joystick.svg" alt="" />
           </div>
         </div>
 
         <div
-          class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+          class="flex flex-col items-center w-50 h-70 space-y-10 rounded-2xl shadow-lg"
         >
           <div class="lg:text-xl mt-5"><h2>Artificial Intelligence</h2></div>
-          <div class="w-30 h-30">
+          <div class="w-25 h-25">
             <img src="../assets/brain.svg" alt="" />
           </div>
         </div>
 
         <div
-          class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+          class="flex flex-col items-center w-50 h-70 space-y-10 rounded-2xl shadow-lg"
         >
-          <div class="lg:text-xl mt-5"><h2>Data Science and Analytics</h2></div>
-          <div class="w-30 h-30">
+          <div class="lg:text-xl mt-5"><h2>Data Science</h2></div>
+          <div class="w-25 h-25">
             <img src="../assets/hosting-server.svg" alt="" />
           </div>
         </div>
 
         <div
-          class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+          class="flex flex-col items-center w-50 h-70 space-y-10 rounded-2xl shadow-lg"
         >
           <div class="lg:text-xl mt-5"><h2>UI and UX</h2></div>
-          <div class="w-30 h-30">
+          <div class="w-25 h-25">
             <img src="../assets/selected.svg" alt="" />
           </div>
         </div>
 
         <div
-          class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+          class="flex flex-col items-center w-50 h-70 space-y-10 rounded-2xl shadow-lg"
         >
           <div class="lg:text-xl mt-5"><h2>Project Managment</h2></div>
-          <div class="w-30 h-30">
+          <div class="w-25 h-25">
             <img src="../assets/rocket-launch.svg" alt="" />
           </div>
         </div>
