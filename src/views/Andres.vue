c291d9da19e00efe27150c05fc2b807bcdf0132e
M4rteaga <m4rteagag@gmail.com>
2021-05-25 18:30:20 -0500

    consumo api de las imagenes

--- src/views/Andres.vue
+++ src/views/Andres.vue
@@ -23,15 +23,39 @@
 			class="relative w-full flex bg-base-grey mt-16 justify-center"
 		>
 			<div
-				class="absolute flex w-46 h-46 z-20 overflow-hidden transform rotate-180 right-0"
+				class="
+					absolute
+					flex
+					w-46
+					h-46
+					z-20
+					overflow-hidden
+					transform
+					rotate-180
+					right-0
+				"
 			>
 				<CircleGreenToPink
-					class="w-46 h-46 backdrop-blur transform translate-y-23 -translate-x-12"
+					class="
+						w-46
+						h-46
+						backdrop-blur
+						transform
+						translate-y-23
+						-translate-x-12
+					"
 				/>
 			</div>
 			<div class="absolute flex w-18 h-18 overflow-hidden bottom-0 left-0">
 				<CircleBlueToGreen
-					class="w-18 h-18 backdrop-blur transform -translate-x-1/2 translate-y-1/2"
+					class="
+						w-18
+						h-18
+						backdrop-blur
+						transform
+						-translate-x-1/2
+						translate-y-1/2
+					"
 				/>
 			</div>
 			<div class="my-8 flex flex-col items-center justify-center w-full">
@@ -46,7 +70,15 @@
 
 		<div
 			id="skill"
-			class="relative w-full flex flex-col bg-indigo-600 justify-center items-cente px-10"
+			class="
+				relative
+				w-full
+				flex flex-col
+				bg-indigo-600
+				justify-center
+				items-cente
+				px-10
+			"
 		>
 			<!-- Skills set -->
 			<div class="flex flex-col justify-center my-8 items-center">
@@ -67,7 +99,15 @@
 			<PortfolioMainText text="Interests" class="text-black" />
 			<div class="w-full flex flex-wrap justify-around items-start">
 				<div
-					class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+					class="
+						flex flex-col
+						items-center
+						w-65
+						h-70
+						space-y-10
+						rounded-2xl
+						shadow-lg
+					"
 				>
 					<div class="lg:text-2xl mt-5"><h2>Game Development</h2></div>
 					<div class="w-25 h-25">
@@ -76,7 +116,15 @@
 				</div>
 
 				<div
-					class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+					class="
+						flex flex-col
+						items-center
+						w-65
+						h-70
+						space-y-10
+						rounded-2xl
+						shadow-lg
+					"
 				>
 					<div class="lg:text-2xl mt-5"><h2>Artificial Intelligence</h2></div>
 					<div class="w-25 h-25">
@@ -85,7 +133,15 @@
 				</div>
 
 				<div
-					class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+					class="
+						flex flex-col
+						items-center
+						w-65
+						h-70
+						space-y-10
+						rounded-2xl
+						shadow-lg
+					"
 				>
 					<div class="lg:text-2xl mt-5"><h2>Data Science</h2></div>
 					<div class="w-25 h-25">
@@ -94,7 +150,15 @@
 				</div>
 
 				<div
-					class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+					class="
+						flex flex-col
+						items-center
+						w-65
+						h-70
+						space-y-10
+						rounded-2xl
+						shadow-lg
+					"
 				>
 					<div class="lg:text-2xl mt-5"><h2>UI and UX</h2></div>
 					<div class="w-25 h-25">
@@ -103,7 +167,15 @@
 				</div>
 
 				<div
-					class="flex flex-col items-center w-65 h-70 space-y-10 rounded-2xl shadow-lg"
+					class="
+						flex flex-col
+						items-center
+						w-65
+						h-70
+						space-y-10
+						rounded-2xl
+						shadow-lg
+					"
 				>
 					<div class="lg:text-2xl mt-5"><h2>Project Managment</h2></div>
 					<div class="w-25 h-25">
@@ -112,7 +184,32 @@
 				</div>
 			</div>
 		</div>
+		<div class="flex flex-col items-center">
+			<h1 class="text-xl font-bold">Laravel Instagram API</h1>
+			<div class="flex flex-row my-5 justify-center items-center">
+				<h3 class="text-center align-middle">{{ user }}</h3>
+				<a
+					href="http://127.0.0.1:8000/"
+					class="bg-blue-700 text-white py-2 px-3 ml-4 rounded-xl"
+				>
+					Follow
+				</a>
+			</div>
 
+			<div class="flex flex-row mx-5 flex-wrap space-y-4 space-x-6">
+				<ul v-for="(res, index) in response[0]">
+					<div class="flex flex-row justify-center items-center">
+						<img
+							class="w-68 h-52 rounded-xl"
+							:src="'http://127.0.0.1:8000/' + res.image"
+							:alt="res.image"
+						/>
+						<p>{{ res.caption }}</p>
+					</div>
+				</ul>
+			</div>
+		</div>
+		<!-- consumo api de laravel para mostrar mis publicaciones -->
 		<div
 			class="w-full flex flex-col items-center justify-center mb-20 mt-10 px-10"
 		>
@@ -121,7 +218,7 @@
 	</main>
 </template>
 
-<script setup>
+<script setup lang="ts">
 	import ContactForm from '../components/portfolio/contactForm.vue';
 	import CircleBlueToGreen from '../components/CircleBlueToGreen.vue';
 	import CircleGreenToPink from '../components/CircleGreenToPink.vue';
@@ -137,6 +234,14 @@
 	import PortfolioGalleryFotos from '../components/portfolio/gallery/PortfolioGalleryFotos.vue';
 	import SkillsTable from '../components/portfolio/Skill/SkillsTable.vue';
 	import SkillsType from '../components/portfolio/Skill/SkillType.vue';
+
+	import axios from 'axios';
+
+	import { reactive, ref } from 'vue';
+
+	let response = ref();
+
+	const user = 'andresvelez11';
 
 	const languages = [
 		{
@@ -160,4 +265,10 @@
 			level: 'w-1/2',
 		},
 	];
+
+	//consumo la api
+	axios.get(`http://localhost/api/user/posts/${user}`).then((res) => {
+		console.log(res);
+		response.value = res.data;
+	});
 </script>
